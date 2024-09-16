ddf8701490d7e93fe6db2d56b0d5542b0f51f112
= <jamesguilbeau8@gmail.com>
2024-09-15 19:59:53 -0500

    Functionality

---
+++ src/chip8.rs
@@ -0,0 +1,403 @@
+use rand::{rngs::SmallRng, SeedableRng, RngCore};
+
+pub const DISPLAY_WIDTH: usize = 64;
+pub const DISPLAY_HEIGHT: usize = 32;
+
+const FONT: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0,
+    0x20, 0x60, 0x20, 0x20, 0x70,
+    0xF0, 0x10, 0xF0, 0x80, 0xF0,
+    0xF0, 0x10, 0xF0, 0x10, 0xF0,
+    0x90, 0x90, 0xF0, 0x10, 0x10,
+    0xF0, 0x80, 0xF0, 0x10, 0xF0,
+    0xF0, 0x80, 0xF0, 0x90, 0xF0,
+    0xF0, 0x10, 0x20, 0x40, 0x40,
+    0xF0, 0x90, 0xF0, 0x90, 0xF0,
+    0xF0, 0x90, 0xF0, 0x10, 0xF0,
+    0xF0, 0x90, 0xF0, 0x90, 0x90,
+    0xE0, 0x90, 0xE0, 0x90, 0xE0,
+    0xF0, 0x80, 0x80, 0x80, 0xF0,
+    0xE0, 0x90, 0x90, 0x90, 0xE0,
+    0xF0, 0x80, 0x80, 0x80, 0xF0,
+    0xF0, 0x80, 0xF0, 0x80, 0x80
+];
+
+pub struct Chip8 {
+    display: [[bool; DISPLAY_WIDTH]; DISPLAY_HEIGHT],
+    memory: [u8; 4096],
+    stack: Vec<u16>,
+    pc: u16,
+    registers: [u8; 16],
+    index_register: u16,
+    delay_timer: u8,
+    sound_timer: u8,
+    rng: SmallRng,
+    input: [bool; 16],
+}
+
+impl Chip8 {
+    pub fn new() -> Chip8 {
+        let mut chip8 = Chip8 {
+            display: [[false; DISPLAY_WIDTH]; DISPLAY_HEIGHT],
+            memory: [0; 4096],
+            stack: Vec::new(),
+            pc: 0x200,
+            registers: [0; 16],
+            index_register: 0,
+            delay_timer: 0,
+            sound_timer: 0,
+            rng: SmallRng::from_entropy(),
+            input: [false; 16],
+        };
+
+        chip8.load_to_memory(&FONT, 0x050);
+
+        //chip8.execute_instruction(0x6055);
+        //chip8.execute_instruction(0x7001);
+
+        chip8
+    }
+    
+    // Is called for every CPU cycle, which varies depending
+    // on settings
+    pub fn update(&mut self) {
+        let instruction = self.fetch_instruction();
+        self.execute_instruction(instruction);
+    }
+    
+    // Is called at a rate of approximately 60Hz
+    pub fn draw(&mut self) {
+        // Timers are decremented in draw phase because they
+        // should decrement 60 times per second
+        if self.delay_timer > 0 { self.delay_timer -= 1; }
+        if self.sound_timer > 0 { self.sound_timer -= 1; }
+    }
+    
+    pub fn load_to_memory(&mut self, data: &[u8], start_pos: usize) {
+        for i in 0..data.len() {
+            self.memory[start_pos+i] = data[i];
+        }
+    }
+
+    pub fn get_display(&self) -> &[[bool; DISPLAY_WIDTH]; DISPLAY_HEIGHT] {
+        &self.display
+    }
+    
+    pub fn get_sound_timer(&self) -> u8 {
+        self.sound_timer
+    }
+
+    fn get_pixel(&self, x: usize, y: usize) -> bool {
+        self.display[y][x]
+    }
+
+    fn set_pixel(&mut self, x: usize, y: usize, value: bool) {
+        self.display[y][x] = value;
+    }
+
+    pub fn set_key(&mut self, key: usize, value: bool) {
+        self.input[key] = value;
+    }
+
+    pub fn fetch_instruction(&mut self) -> u16 {
+        let b1 = self.memory[self.pc as usize] as u16;
+        let b2 = self.memory[(self.pc+1) as usize] as u16;
+        let instruction = (b1 << 8) + b2;
+
+        //println!("{:#b} + {:#b} = {:#b}", b1, b2, instruction);
+
+        self.pc += 2;
+
+        instruction
+    }
+
+    pub fn execute_instruction(&mut self, instruction: u16) {
+        // Split instruction into four half-bytes
+        let n1 = ((instruction >> 12) & 0xf) as u8;
+        let n2 = ((instruction >> 8) & 0xf) as u8;
+        let n3 = ((instruction >> 4) & 0xf) as u8;
+        let n4 = (instruction & 0xf) as u8;
+
+        // Last two half-bytes of instruction
+        let n3n4 = (instruction &0xff) as u8;
+
+        // Last three half-bytes of instruction
+        let n2n3n4 = (instruction &0xfff) as u16;
+
+        //println!("{:x}, {:x}, {:x}, {:x}", n1, n2, n3, n4);
+        
+        // Match instruction to opcode
+        match n1 {
+            0x0 => {
+                // 00E0: Clear screen
+                if instruction == 0x00e0 {
+                    self.clear_screen();
+                }
+                // 00EE: Return from subroutine
+                else if instruction == 0x00ee {
+                    self.pc = self.stack.pop().unwrap();
+                }
+            },
+            // 1NNN: Jump
+            0x1 => {
+                // Decrement PC to cancel out increment during fetch stage
+                //self.pc -= 2;
+
+                self.pc = n2n3n4;
+            },
+            // 2NNN: Call subroutine
+            0x2 => {
+                
+                // Decrement PC to cancel out increment during fetch stage
+                //self.pc -= 2;
+                
+                self.stack.push(self.pc);
+
+                self.pc = n2n3n4;
+                
+            },
+            // 3XNN: Conditional (register equal to value)
+            0x3 => {
+                if self.registers[n2 as usize] == n3n4 {
+                    self.pc += 2;
+                }
+            },
+            // 4XNN: Conditional (register not equal to value)
+            0x4 => {
+                if self.registers[n2 as usize] != n3n4 {
+                    self.pc += 2;
+                }
+            },
+            // 5XY0: Conditional (registers equal)
+            0x5 => {
+                if self.registers[n2 as usize] == self.registers[n3 as usize] {
+                    self.pc += 2;
+                }
+            },
+            // 6XNN: Set
+            0x6 => {
+                self.registers[n2 as usize] = n3n4;
+            },
+            // 7XNN: Add
+            0x7 => {
+                // Convert to u16 to handle overflow then convert back to u8 and add
+                let mut buffer: u16 = self.registers[n2 as usize] as u16;
+                buffer += n3n4 as u16;
+                self.registers[n2 as usize] = (buffer % 256) as u8;
+            },
+            0x8 => {
+                match n4 {
+                    // 8XY0: Set
+                    0x0 => {
+                        self.registers[n2 as usize] = self.registers[n3 as usize];
+                    },
+                    // 8XY1: Binary OR
+                    0x1 => {
+                        self.registers[n2 as usize] |= self.registers[n3 as usize];
+                    },
+                    // 8XY2: Binary AND
+                    0x2 => {
+                        self.registers[n2 as usize] &= self.registers[n3 as usize];
+                    },
+                    // 8XY3: Logical XOR
+                    0x3 => {
+                        self.registers[n2 as usize] ^= self.registers[n3 as usize];
+                    },
+                    // 8XY4: Add
+                    0x4 => {
+                        let mut buffer: u16 = self.registers[n2 as usize] as u16;
+                        buffer += self.registers[n3 as usize] as u16;
+                        self.registers[0xf] = if buffer > 255 { 1 } else { 0 };
+                        self.registers[n2 as usize] = (buffer % 256) as u8;
+                    },
+                    // 8XY5: Subtract (VX - VY)
+                    0x5 => {
+                        if self.registers[n2 as usize] >= self.registers[n3 as usize] {
+                            self.registers[n2 as usize] -= self.registers[n3 as usize];
+                            self.registers[0xf] = 1;
+                        } else {
+                            self.registers[n2 as usize] = 255 - (self.registers[n3 as usize] - self.registers[n2 as usize]) + 1;
+                            self.registers[0xf] = 0;
+                        }
+                    },
+                    // 8XY6: Shift right
+                    0x6 => {
+                        //self.registers[n2 as usize] = self.registers[n3 as usize];
+                        self.registers[0xf] = self.registers[n2 as usize] & 1;
+                        self.registers[n2 as usize] >>= 1;
+                    }
+                    // 8XY7: Subtract (VY - VX)
+                    0x7 => {
+                        if self.registers[n3 as usize] >= self.registers[n2 as usize] {
+                            self.registers[n3 as usize] -= self.registers[n2 as usize];
+                            self.registers[0xf] = 1;
+                        } else {
+                            self.registers[n3 as usize] = 255 - (self.registers[n2 as usize] - self.registers[n3 as usize]) + 1;
+                            self.registers[0xf] = 0;
+                        }
+                    },
+                    // 8XYE: Shift left
+                    0xe => {
+                        //self.registers[n2 as usize] = self.registers[n3 as usize];
+                        self.registers[0xf] = (self.registers[n2 as usize] >> 7) & 1;
+                        self.registers[n2 as usize] <<= 1;
+                    }
+                    _ => {}
+                }
+            },
+            // 9XY0: Conditional (registers not equal)
+            0x9 => {
+                if self.registers[n2 as usize] != self.registers[n3 as usize] {
+                    self.pc += 2;
+                }
+            },
+            // ANNN: Set index
+            0xa => {
+                self.index_register = n2n3n4;
+            },
+            // BNNN: Jump with offset
+            0xb => {
+                self.pc = n2n3n4 + self.registers[0] as u16;
+            },
+            // CXNN: Random
+            0xc => {
+                // Generate 4-bit random integer
+                let random = (self.rng.next_u32() & 0b1111) as u8;
+
+                self.registers[n2 as usize] = random & n3n4;
+            },
+            // DXYN: Display
+            0xd => {
+                let x = self.registers[n2 as usize] % DISPLAY_WIDTH as u8;
+                let y = self.registers[n3 as usize] % DISPLAY_HEIGHT as u8;
+                self.draw_sprite(x, y, n4);
+            },
+            0xe => {
+                match n3n4 {
+                    // EX9E: Skip if key
+                    0x9e => {
+                        let key = self.registers[n2 as usize];
+                        if self.input[key as usize] {
+                            self.pc += 2
+                        }
+                    },
+                    // EXA1: Skip if not key
+                    0xa1 => {
+                        let key = self.registers[n2 as usize];
+                        if !self.input[key as usize] {
+                            self.pc += 2
+                        }
+                    },
+                    _ => {},
+                }
+            },
+            0xf => {
+                match n3n4 {
+                    // FX07: Set register to delay timer
+                    0x07 => {
+                        self.registers[n2 as usize] = self.delay_timer;
+                    },
+                    // FX15: Set delay timer to register
+                    0x15 => {
+                        self.delay_timer = self.registers[n2 as usize];
+                    },
+                    // FX18: Set sound timer to register
+                    0x18 => {
+                        self.sound_timer = self.registers[n2 as usize];
+                    },
+                    // FX1E: Add to index
+                    0x1e => {
+                        self.index_register += self.registers[n2 as usize] as u16;
+                    },
+                    // FX0A: Wait for key
+                    0x0a => {
+                        for key in self.input {
+                            if key { break; }
+
+                            self.pc -= 2;
+                        }
+                    },
+                    // FX29: Font character
+                    0x29 => {
+                        // Desired hexadecimal character to locate in font
+                        let char = self.registers[n2 as usize] & 0b1111;
+
+                        self.index_register = self.memory[(0x050 + 5 * char) as usize] as u16;
+                    },
+                    // FX33: Binary-coded decimal conversion
+                    0x33 => {
+                        let num = self.registers[n2 as usize];
+
+                        //println!("{}", num);
+
+                        let index = self.index_register as usize;
+                        self.memory[index] = (num / 100) % 10;
+                        self.memory[index + 1] = (num / 10) % 10;
+                        self.memory[index + 2] = num % 10;
+
+                        //println!("{} {} {}", self.memory[index], self.memory[index+1], self.memory[index+2]);
+                    },
+                    // FX55: Store registers in memory
+                    0x55 => {
+                        for i in 0..n2+1 {
+                            self.memory[(self.index_register + i as u16) as usize] = self.registers[i as usize];
+                        }
+                    },
+                    // FX65: Load registers from memory
+                    0x65 => {
+                        for i in 0..n2+1 {
+                            self.registers[i as usize] = self.memory[(self.index_register + i as u16) as usize];
+                        }
+                    },
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+    }
+
+    fn clear_screen(&mut self) {
+        for i in 0..DISPLAY_HEIGHT {
+            for j in 0..DISPLAY_WIDTH {
+                self.set_pixel(j, i, false);
+            }
+        }
+    }
+
+    fn draw_sprite(&mut self, x: u8, y: u8, height: u8) {
+        self.registers[15] = 0;
+
+        let mut index = self.index_register as usize;
+        for i in 0..height {
+            if y+i > DISPLAY_HEIGHT as u8 { break; }
+
+            let byte = self.memory[index];
+            for j in 0..8 {
+                // Stop drawing row if outside display bounds
+                if x+j > DISPLAY_WIDTH as u8 { break; }
+
+                // Get pixel value from sprite data in memory
+                let sprite_value = ((byte >> (7 - j)) & 1) == 1;
+
+                if sprite_value {
+                    // Flip corresponding bit on screen
+                    let screen_value = self.get_pixel((x+j) as usize, (y+i) as usize);
+                    self.set_pixel((x+j) as usize, (y+i) as usize, !screen_value);
+
+                    if screen_value { self.registers[15] = 1; }
+                }
+            }
+
+            index += 1;
+        }
+    }
+
+    // Debug methods
+    pub fn print_memory(&self) {
+        for (i, val) in self.memory.iter().enumerate() {
+            if i % 10 == 0 { println!(); }
+            print!("{:3x}: {:<2x}, ", i, val);
+        }
+        println!("\n");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,141 @@
+use std::{env, fs, process, thread, time};
+use minifb::{Key, Window, WindowOptions};
+use rodio::{OutputStream, Sink};
+use rodio::source::{SineWave, Source};
+
+mod chip8;
+
+const CYCLES_PER_SECOND: f32 = 700.0;
+const MICROSECONDS_PER_CYCLE: u128 = ((1.0 / CYCLES_PER_SECOND) * 1_000_000.0) as u128;
+
+const INPUT_MAP: [Key; 16] = [
+    Key::X, /* 0 */
+    Key::Key1, /* 1 */
+    Key::Key2, /* 2 */
+    Key::Key3, /* 3 */
+    Key::Q, /* 4 */
+    Key::W, /* 5 */
+    Key::E, /* 6 */
+    Key::A, /* 7 */
+    Key::S, /* 8 */
+    Key::D, /* 9 */
+    Key::Z, /* A */
+    Key::C, /* B */
+    Key::Key4, /* C */
+    Key::R, /* D */
+    Key::F, /* E */
+    Key::V, /* F */
+    ];
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = parse_arguments(&args).unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
+
+    let mut chip8 = chip8::Chip8::new();
+
+    let mut window = initialize_window();
+
+    let path = format!("roms/{}", config.rom_path);
+    let program = fs::read(&path).unwrap_or_else(|e| {
+        println!("Error: file not found at {path}");
+        process::exit(1);
+    });
+    chip8.load_to_memory(&program, 0x200);
+    //chip8.print_memory();
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    let sin = SineWave::new(440.0).take_duration(time::Duration::from_secs_f32(0.25));
+
+    let mut display_timer = time::SystemTime::now();
+    let mut cycle_timer = time::SystemTime::now();
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        // Get keyboard input and send to chip8
+        for i in 0..INPUT_MAP.len() {
+            chip8.set_key(i, window.is_key_down(INPUT_MAP[i]));
+        }
+
+        // Call next chip8 CPU cycle
+        chip8.update();
+
+        if display_timer.elapsed().unwrap().as_micros() > 16600 {
+            // Print FPS to console
+            //println!("{}", 1.0 / display_timer.elapsed().unwrap().as_secs_f64());
+
+            // Call chip8 draw phase
+            chip8.draw();
+
+            // Update window
+            let buffer = translate_display(chip8.get_display());
+            window
+                .update_with_buffer(&buffer, chip8::DISPLAY_WIDTH, chip8::DISPLAY_HEIGHT)
+                .unwrap();
+
+            // Play audio
+            if sink.len() <= 1 { sink.append( SineWave::new(440.0).take_duration(time::Duration::from_secs_f32(0.25)) ) };
+            if chip8.get_sound_timer() > 0 { sink.play(); }
+            else { sink.pause(); }
+
+            // Reset display timer
+            display_timer = time::SystemTime::now();
+        }
+
+        thread::sleep(time::Duration::from_micros(MICROSECONDS_PER_CYCLE as u64));
+    }
+}
+
+fn initialize_window() -> Window {
+    let mut window = Window::new(
+        "chip8-rs",
+        chip8::DISPLAY_WIDTH,
+        chip8::DISPLAY_HEIGHT,
+        WindowOptions {
+            resize: false,
+            scale: minifb::Scale::X8,
+            ..WindowOptions::default()
+        }
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.limit_update_rate(None);
+
+    window
+}
+
+// Translates the chip8's monochrome display buffer to a buffer that can be sent to minifb
+fn translate_display(chip8_buffer: &[[bool; chip8::DISPLAY_WIDTH]; chip8::DISPLAY_HEIGHT])
+    -> [u32; chip8::DISPLAY_WIDTH * chip8::DISPLAY_HEIGHT] {
+    let mut window_buffer = [0; chip8::DISPLAY_WIDTH * chip8::DISPLAY_HEIGHT];
+
+    for i in 0..chip8_buffer.len() {
+        for j in 0..chip8_buffer[i].len() {
+            window_buffer[j + (i * chip8::DISPLAY_WIDTH)] = if chip8_buffer[i][j] == true {
+                0xFFFFFFFF
+            } else {
+                0x00000000
+            };
+        }
+    }
+
+    window_buffer
+}
+
+fn parse_arguments(args: &[String]) -> Result<Config, &'static str> {
+    if args.len() < 2 {
+        return Err("Error: no ROM path specified");
+    }
+
+    let rom_path = args[1].clone();
+
+    Ok(Config { rom_path })
+}
+
+struct Config {
+    rom_path: String,
+}
+
